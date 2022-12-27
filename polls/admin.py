d52dc25ab270016d90f1f0a005e76118b7ec4164
= <=>
2022-12-26 22:12:08 -0300

    new commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceRow(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question: ", {"fields":["question_text"]}),
+        ("Date: ", {"fields":["pub_date"]}),
+
+    ]
+    inlines = [ChoiceRow]
+
+admin.site.register(Question, QuestionAdmin)
+
+
